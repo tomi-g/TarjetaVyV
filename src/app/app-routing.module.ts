dc2493f325835c3242bcf53883200bceca7b4419
tomi-g <tomigarci19@outlook.com>
2023-03-13 17:47:03 -0300

    Nuevo rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Vale&Vale', component: InicioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
